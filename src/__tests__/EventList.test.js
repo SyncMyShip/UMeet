b3ecba2c152abcec809c2cae0c6b06c648742d9f
Simba <jfrancis1195@gmail.com>
2024-09-07 14:52:03 -0400

    passing before exercise

---
+++ src/__tests__/EventList.test.js
@@ -0,0 +1,25 @@
+// src/__tests__/EventList.test.js
+
+import { render } from '@testing-library/react';
+import EventList from '../components/EventList';
+import { getEvents } from '../api';
+
+describe('<EventList /> component', () => {
+    
+    test('"list" role exists', () => {
+        const EventListComponent = render(<EventList />);
+        expect(EventListComponent.queryByRole("list")).toBeInTheDocument();
+    })
+
+    test('renders correct number of events', () => {
+        const EventListComponent = render(<EventList events={[{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }]} />);
+        expect(EventListComponent.getAllByRole("listitem")).toHaveLength(4);
+    });
+
+    test('renders correct number of events', async () => {
+        const EventListComponent = render(<EventList />)
+        const allEvents = await getEvents(); 
+        EventListComponent.rerender(<EventList events={allEvents} />);
+    expect(EventListComponent.getAllByRole("listitem")).toHaveLength(allEvents.length);
+      });
+});
