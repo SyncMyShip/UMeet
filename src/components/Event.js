0865bb895be2907fb981bf4a6828f0b9718d9800
Simba <jfrancis1195@gmail.com>
2024-09-09 22:44:39 -0400

    another css update

--- src/components/Event.js
+++ src/components/Event.js
@@ -5,7 +5,7 @@ import { useState } from "react";
 const Event = ({ event }) => {
     const [showDetails, setShowDetails] = useState(false)
     return (
-        <li className="event-list">
+        <li>
             <div className="event-summary">
                 <h2>{event.summary}</h2>
                 <p>{event.location}</p>
