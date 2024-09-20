08f917f27a100fd9cb589e2f94b8ea4408fd35f3
Simba <jfrancis1195@gmail.com>
2024-09-19 20:54:30 -0400

    reorganize citysearch

--- src/components/CitySearch.js
+++ src/components/CitySearch.js
@@ -8,10 +8,6 @@ const CitySearch = ({ allLocations, setCurrentCity, setInfoAlert }) => {
     const [query, setQuery ] = useState("");
     const [suggestions, setSuggestions] = useState([]);
 
-   useEffect(() => {
-       setSuggestions(allLocations);
-     }, [`${allLocations}`]);
-
     const handleInputChanged = (event) => {
         const value = event.target.value;
         const filteredLocations = allLocations ? allLocations.filter((location) => {
@@ -38,6 +34,10 @@ const CitySearch = ({ allLocations, setCurrentCity, setInfoAlert }) => {
         setCurrentCity(value);
         setInfoAlert("");
         };
+
+    useEffect(() => {
+        setSuggestions(allLocations);
+        }, [`${allLocations}`]);
     
    return (
      <div id="city-search">
